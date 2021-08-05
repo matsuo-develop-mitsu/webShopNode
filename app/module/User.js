5cc83801f122fdc06c590f416b2eb580da5887c4
matsuo-develop-mitsu <matsuo.develop+mitsu@gmail.com>
2021-08-05 14:55:36 +0900

    ・MVCモデルを意識
    MVCモデルを意識して、ログイン機能をリファクタリング。

--- app/module/User.js
+++ app/module/User.js
@@ -1,12 +1,31 @@
 const mysql = require("mysql2");
-const dbConfig = require("../dbConfig");
+const { dbConfig } = require("../dbConfig");
 const userTable = "User";
 const userDataIndex = 0;
 
 module.exports = {
-  getLoginUser: async function () {
-    let result = null;
+  getLoginUser: async function (email, password) {
+    let result;
     const connection = mysql.createConnection(dbConfig);
-    return connection.promise().query(`SELECT * FROM ${userTable}`);
+    await connection
+      .promise()
+      .query(`SELECT * FROM ${userTable} WHERE mail = ? and password = ?`, [
+        email,
+        password,
+      ])
+      .then((data) => {
+        if (data) {
+          result = data[userDataIndex];
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      })
+      .finally(() => {
+        if (connection) {
+          connection.end();
+        }
+      });
+    return result;
   },
 };
