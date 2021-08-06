1397a51ffdd527d3dc1f0a2ca6d5b8b022681a01
matsuo-develop-mitsu <matsuo.develop+mitsu@gmail.com>
2021-08-06 14:17:38 +0900

    ログイン機能を修正

--- app/controller/UserController.js
+++ app/controller/UserController.js
@@ -11,13 +11,18 @@ module.exports = {
     });
     request.on("end", () => {
       loginUser = qs.parse(loginUser);
-      User.getLoginUser(loginUser.email, loginUser.password).then((result) => {
-        if (result) {
-          response.redirect("/list");
-        } else {
-          response.redirect("/");
-        }
-      });
+
+      // フロントでメールアドレスかパスワードがからであればDBに登録されているかどうかの処理を行う
+      if (!loginUser.email || !loginUser.password) {
+        response.redirect("/");
+      } else {
+        User.getLoginUser(loginUser.email, loginUser.password).then(
+          (result) => {
+            // メールアドレスとパスワードがDBに登録されているユーザーと一致すれば、リスト画面に飛ぶ
+            result ? response.redirect("/list") : response.redirect("/");
+          }
+        );
+      }
     });
   },
   doRegistUser: function (request, response) {
